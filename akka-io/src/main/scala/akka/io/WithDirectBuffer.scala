8f92fe821c8c123f92d0c0c48dd7e0d5566eb6a2
Johannes Rudolph <johannes_rudolph@gmx.de>
2013-01-15 16:31:34 +0100

    documentation for WithDirectBuffer

--- akka-io/src/main/scala/akka/io/WithDirectBuffer.scala
+++ akka-io/src/main/scala/akka/io/WithDirectBuffer.scala
@@ -3,6 +3,11 @@ package akka.io
 import java.nio.ByteBuffer
 import akka.actor.Actor
 
+/**
+ * Allows an actor to get a thread local direct buffer of a size defined in the
+ * configuration of the actor system. An underlying assumption is that all of
+ * the threads which call `getDirectBuffer` are owned by the actor system.
+ */
 trait WithDirectBuffer { _: Actor ⇒
   def getDirectBuffer(): ByteBuffer = {
     val result = WithDirectBuffer.threadLocalBuffer.get()
